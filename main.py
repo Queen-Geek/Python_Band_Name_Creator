4c04b8c39a2bf09d7c0c58f16a340a6c54d33fa7
Queen-Geek <QueenGeekWorld@gmail.com>
2022-02-15 19:44:42 -0500

    main.py
    Band Name Creator

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to What's My Band Name!")
+city = input("What city did you grow up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your new band name is " + city + " " + pet)
